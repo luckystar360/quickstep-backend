4fef6613f8e3f86130af647c7b1c136a5b060459
luckystar360 <ngocnhat95@gmail.com>
2024-06-14 23:57:46 +0700

    .

--- src/restful/controllers/mobile_info.ts
+++ src/restful/controllers/mobile_info.ts
@@ -33,7 +33,7 @@ export default class MobileInfoController {
         mobileInfo = await MobileInfo.create({ ...req.body });
       } else {
         mobileInfo.updatedAt = new Date();
-    
+        mobileInfo = {...mobileInfo.toObject(), ...req.body};
         await MobileInfo.findByIdAndUpdate(mobileInfo?.id, mobileInfo!);
       }
       const trackerIds = existUser.trackerIdList?.map((item) => item.id) ?? [];
