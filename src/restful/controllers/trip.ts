80abe9b4b820f5e05a8e74428f958e7d70f56a3d
luckystar360 <ngocnhat95@gmail.com>
2024-06-17 22:28:27 +0700

    .

--- src/restful/controllers/trip.ts
+++ src/restful/controllers/trip.ts
@@ -106,12 +106,16 @@ export default class TripController {
     try {
       const { markerId, lat, lon, name, enable } = req.body;
       let marker = await Marker.findById(markerId);
-      if (!marker) throw new Error("Marker not found"); 
-      marker.location = {...(marker.location as any).toObject(), lat, lon};
+      if (!marker) throw new Error("Marker not found");
+      if (lat != null) marker.location.lat = lat;
+      if (lon != null) marker.location.lon = lon;
+      if (name != null) marker.name = name;
+      if (enable != null) marker.enable = enable;
+
       marker.updatedAt = new Date();
-      marker = { ...marker.toObject(), ...({ name, enable } as any) };
+
       await Marker.findByIdAndUpdate(markerId, marker!);
-      return respond.success(201, {
+      return respond.success(200, {
         message: "edit marker successfully!",
         data: marker,
       });
